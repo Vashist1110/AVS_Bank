d474bbf322299cc162a94801cb6aa98d923c7ad4
AyushVerma <114309983+yushA987@users.noreply.github.com>
2025-10-15 09:54:01 +0000

    login route updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './pages/Home';
 import UserDashboard from './pages/UserDashboard';
 import CreateAccount from './pages/Create_Account';
+import Login from './pages/User_Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/user-dashboard" element={<UserDashboard />} />
           <Route path="/create_account" element={<CreateAccount />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
